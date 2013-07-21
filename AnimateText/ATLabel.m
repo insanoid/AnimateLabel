3252d815c76e9314df6abe56c1cba5692dcfdd44
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2013-07-21 21:47:55 +0530

    Filename changes.

--- AnimateText/ATLabel.m
+++ AnimateText/ATLabel.m
@@ -1,13 +1,31 @@
 //
-//  EFLabel.m
+//  ATLabel.m
 //
-//  Created by User on 12/03/13.
+// Created by Karthikeya Udupa on 7/14/13.
+// Copyright (c) 2012 Karthikeya Udupa K M All rights reserved.
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 //
 
-#import "EFLabel.h"
+#import "ATLabel.h"
 
-@implementation EFLabel
+@implementation ATLabel
 @synthesize wordList = _wordList;
 @synthesize duration = _duration;
 
@@ -65,7 +83,7 @@
             self.alpha = 1.0;
             self.text = [self.wordList objectAtIndex:[num intValue]];
         } completion:^(BOOL finished) {
-            //sleep(5);
+            
         }];
     }];
     
